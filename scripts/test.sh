b36603a0702fbd1d775623f14c3ee77b0015b73a
wdecoster <decosterwouter@gmail.com>
2017-11-10 09:47:58 +0100

    got to specify path to test files

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,6 @@ git clone https://github.com/wdecoster/nanotest.git
 NanoComp -h
 
 
-NanoComp --bam alignment.bam alignment.bam alignment.bam --outdir compare-bams -f pdf
+NanoComp --bam nanotest/alignment.bam nanotest/alignment.bam nanotest/alignment.bam --outdir compare-bams -f pdf -n run1 run2 run3
 
-NanoComp --fastq reads.fastq.gz reads.fastq.gz --names run1 run2 --plot box
+NanoComp --fastq nanotest/reads.fastq.gz nanotest/reads.fastq.gz --names run1 run2 --plot box
