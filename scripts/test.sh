319d0dcf8d18ddd9641208c02183b92d8aec23ec
wdecoster <decosterwouter@gmail.com>
2017-11-13 14:35:04 +0100

    adding summary to tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,6 @@ set -ev
 git clone https://github.com/wdecoster/nanotest.git
 
 NanoComp -h
-
-
 NanoComp --bam nanotest/alignment.bam nanotest/alignment.bam nanotest/alignment.bam --outdir compare-bams -f pdf -n run1 run2 run3
-
+NanoComp --summary nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt -n A B C
 NanoComp --fastq nanotest/reads.fastq.gz nanotest/reads.fastq.gz --names run1 run2 --plot box
