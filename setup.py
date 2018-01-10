fb9b1ba7778b19d8018c55c88be198f69a886431
wdecoster <decosterwouter@gmail.com>
2018-01-10 16:14:43 +0100

    make dependendent on latest versions of nanoget and nanomath

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
     install_requires=['pandas',
                       'numpy',
                       'nanoplotter>=0.26.0',
-                      'nanoget>=1.1.0',
-                      'nanomath>=0.15.0',
+                      'nanoget>=1.2.2',
+                      'nanomath>=0.15.3',
                       'NanoPlot>=0.19.1'
                       ],
     package_data={'NanoComp': []},
