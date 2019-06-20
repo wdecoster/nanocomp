4102c825797f465d87c74a6d7aa3524d8d84e99c
wdecoster <decosterwouter@gmail.com>
2019-06-20 09:38:47 +0200

    refactoring, duplicating code from nanoplotter

---
+++ nanocomp/utils.py
@@ -0,0 +1,177 @@
+import logging
+import sys
+import seaborn as sns
+from .version import __version__
+from argparse import ArgumentParser, FileType
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pytplot as plt
+
+
+def check_valid_format(figformat):
+    """Check if the specified figure format is valid.
+
+    If format is invalid the default is returned.
+    Probably installation-dependent
+    """
+    fig = plt.figure()
+    if figformat in list(fig.canvas.get_supported_filetypes().keys()):
+        logging.info("Nanoplotter: valid output format {}".format(figformat))
+        return figformat
+    else:
+        logging.info("Nanoplotter: invalid output format {}".format(figformat))
+        sys.stderr.write("Invalid format {}, using default.\n".format(figformat))
+        return "png"
+
+
+def plot_settings(plot_settings, dpi):
+    sns.set(**plot_settings)
+    mpl.rcParams['savefig.dpi'] = dpi
+
+
+def validate_split_runs_file(split_runs_file):
+    """Check if structure of file is as expected and return dictionary linking names to run_IDs."""
+    try:
+        content = [l.strip() for l in split_runs_file.readlines()]
+        if content[0].upper().split('\t') == ['NAME', 'RUN_ID']:
+            return {c.split('\t')[1]: c.split('\t')[0] for c in content[1:] if c}
+        else:
+            sys.exit("ERROR: Mandatory header of --split_runs tsv file not found: 'NAME', 'RUN_ID'")
+            logging.error("Mandatory header of --split_runs tsv file not found: 'NAME', 'RUN_ID'")
+    except IndexError:
+        sys.exit("ERROR: Format of --split_runs tab separated file not as expected")
+        logging.error("ERROR: Format of --split_runs tab separated file not as expected")
+
+
+def change_identifiers(datadf, split_dict):
+    """Change the dataset identifiers based on the names in the dictionary."""
+    for rid, name in split_dict.items():
+        datadf.loc[datadf["runIDs"] == rid, "dataset"] = name
+
+
+def get_args():
+    epilog = """EXAMPLES:
+    NanoComp --bam alignment1.bam alignment2.bam --outdir compare-runs
+    NanoComp --fastq reads1.fastq.gz reads2.fastq.gz reads3.fastq.gz  --names run1 run2 run3
+    """
+    parser = ArgumentParser(
+        description="Compares long read sequencing datasets.",
+        epilog=epilog,
+        formatter_class=utils.custom_formatter,
+        add_help=False)
+    general = parser.add_argument_group(
+        title='General options')
+    general.add_argument("-h", "--help",
+                         action="help",
+                         help="show the help and exit")
+    general.add_argument("-v", "--version",
+                         help="Print version and exit.",
+                         action="version",
+                         version='NanoComp {}'.format(__version__))
+    general.add_argument("-t", "--threads",
+                         help="Set the allowed number of threads to be used by the script",
+                         default=4,
+                         type=int)
+    general.add_argument("-o", "--outdir",
+                         help="Specify directory in which output has to be created.",
+                         default=".")
+    general.add_argument("-p", "--prefix",
+                         help="Specify an optional prefix to be used for the output files.",
+                         default="",
+                         type=str)
+    general.add_argument("--verbose",
+                         help="Write log messages also to terminal.",
+                         action="store_true")
+    general.add_argument("--raw",
+                         help="Store the extracted data in tab separated file.",
+                         action="store_true")
+    general.add_argument("--store",
+                         help="Store the extracted data in a pickle file for future plotting.",
+                         action="store_true")
+    filtering = parser.add_argument_group(
+        title='Options for filtering or transforming input prior to plotting')
+    filtering.add_argument("--readtype",
+                           help="Which read type to extract information about from summary. \
+                             Options are 1D, 2D, 1D2",
+                           default="1D",
+                           choices=['1D', '2D', '1D2'])
+    filtering.add_argument("--maxlength",
+                           help="Drop reads longer than length specified.",
+                           type=int,
+                           metavar='N')
+    filtering.add_argument("--minlength",
+                           help="Drop reads shorter than length specified.",
+                           type=int,
+                           metavar='N')
+    filtering.add_argument("--barcoded",
+                           help="Barcoded experiment in summary format, splitting per barcode.",
+                           action="store_true")
+    filtering.add_argument("--split_runs",
+                           help="File: Split the summary on run IDs and use names in tsv file. "
+                                "Mandatory header fields are 'NAME' and 'RUN_ID'.",
+                           default=False,
+                           type=FileType('r'),
+                           metavar="TSV_FILE")
+    visual = parser.add_argument_group(
+        title='Options for customizing the plots created')
+    visual.add_argument("-f", "--format",
+                        help="Specify the output format of the plots.",
+                        default="png",
+                        type=str,
+                        choices=['eps', 'jpeg', 'jpg', 'pdf', 'pgf', 'png', 'ps',
+                                 'raw', 'rgba', 'svg', 'svgz', 'tif', 'tiff'])
+    visual.add_argument("-n", "--names",
+                        help="Specify the names to be used for the datasets",
+                        nargs="+",
+                        metavar="names")
+    visual.add_argument("-c", "--colors",
+                        help="Specify the colors to be used for the datasets",
+                        nargs="+",
+                        metavar="colors")
+    visual.add_argument("--plot",
+                        help="Which plot type to use: "
+                             "'box', 'violin' (default), 'ridge' (joyplot) or 'false' (no plots)",
+                        type=str,
+                        choices=['violin', 'box', 'ridge', 'false'],
+                        default="violin")
+    visual.add_argument("--title",
+                        help="Add a title to all plots, requires quoting if using spaces",
+                        type=str,
+                        default=None)
+    visual.add_argument("--dpi",
+                        help="Set the dpi for saving images",
+                        type=int,
+                        default=100)
+    target = parser.add_argument_group(
+        title="Input data sources, one of these is required.")
+    mtarget = target.add_mutually_exclusive_group(
+        required=True)
+    mtarget.add_argument("--fasta",
+                         help="Data is in (compressed) fasta format.",
+                         nargs='+',
+                         metavar="file")
+    mtarget.add_argument("--fastq",
+                         help="Data is in (compressed) fastq format.",
+                         nargs='+',
+                         metavar="files")
+    mtarget.add_argument("--summary",
+                         help="Data is in (compressed) summary files generated by \
+                               albacore or guppy.",
+                         nargs='+',
+                         metavar="files")
+    mtarget.add_argument("--bam",
+                         help="Data is in sorted bam files.",
+                         nargs='+',
+                         metavar="files")
+    args = parser.parse_args()
+    if args.names:
+        if not len(args.names) == [len(i) for i in
+                                   [args.fastq, args.summary, args.bam, args.fasta] if i][0]:
+            sys.exit("ERROR: Number of names (-n) should be same as number of files specified!")
+        if len(args.names) != len(set(args.names)):
+            sys.stderr.write("\nWarning: duplicate values in -n/--names detected. ")
+            sys.stderr.write("Datasets with the same name will be merged.\n\n")
+    if args.colors:
+        if not len(args.colors) == [len(i) for i in [args.fastq, args.summary, args.bam] if i][0]:
+            sys.exit("ERROR: Number of colors (-c) should be same as number of files specified!")
+    return args
