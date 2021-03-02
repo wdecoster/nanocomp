d7e01e0d47a70346ada39a48d7c0d48602ca48f7
iliasbukraa <ilias.bukraa@gmail.com>
2021-03-02 18:07:09 +0100

    rewrote functions n50_barplot/output_barplot/violin_or_box_plot to use plotly instead of seaborn/matplotlib

--- nanocomp/compplots.py
+++ nanocomp/compplots.py
@@ -11,112 +11,144 @@ import sys
 import joypy
 
 
-def violin_or_box_plot(df, y, figformat, path, y_name,
-                       title=None, plot="violin", log=False, palette=None):
-    """Create a violin or boxplot from the received DataFrame.
+def violin_or_box_plot(df, y, path, y_name, title=None, plot="violin", log=False):
+    """Create a violin/boxplot/ridge from the received DataFrame.
 
     The x-axis should be divided based on the 'dataset' column,
     the y-axis is specified in the arguments
     """
-    comp = Plot(path=path + "NanoComp_" + y.replace(' ', '_') + '.' + figformat,
+    comp = Plot(path=path + "NanoComp_" + y.replace(' ', '_') + '.html',
                 title="Comparing {}".format(y_name.lower()))
 
     if plot == 'violin':
         logging.info("NanoComp: Creating violin plot for {}.".format(y))
-        process_violin_and_box(ax=sns.violinplot(x="dataset",
-                                                 y=y,
-                                                 data=df,
-                                                 inner=None,
-                                                 cut=0,
-                                                 palette=palette,
-                                                 linewidth=0),
+        
+        ax = go.Figure()
+        
+        for d in df["dataset"].unique():
+            ax.add_trace(go.Violin(x=df['dataset'][df['dataset'] == d],
+                                    y=df[y][df['dataset'] == d],
+                                    name=d,
+                                    points=False))       
+        
+        process_violin_and_boxx(ax,
                                log=log,
                                plot_obj=comp,
                                title=title,
                                y_name=y_name,
-                               figformat=figformat,
                                ymax=np.amax(df[y]))
-    elif plot == 'box':
+    
+    elif plot == 'box':     
         logging.info("NanoComp: Creating box plot for {}.".format(y))
-        process_violin_and_box(ax=sns.boxplot(x="dataset",
-                                              y=y,
-                                              data=df,
-                                              palette=palette),
+        
+        ax = go.Figure()
+        for d in df["dataset"].unique():
+            ax.add_trace(go.Box(y=df[y][df['dataset'] == d],name=d))          
+                         
+        process_violin_and_boxx(ax,
                                log=log,
                                plot_obj=comp,
                                title=title,
                                y_name=y_name,
-                               figformat=figformat,
                                ymax=np.amax(df[y]))
+    
     elif plot == 'ridge':
         logging.info("NanoComp: Creating ridges plot for {}.".format(y))
-        comp.fig, axes = joypy.joyplot(df,
-                                       by="dataset",
-                                       column=y,
-                                       title=title or comp.title,
-                                       x_range=[-0.05, np.amax(df[y])])
-        if log:
-            xticks = [float(i.get_text()) for i in axes[-1].get_xticklabels()]
-            axes[-1].set_xticklabels([10**i for i in xticks])
-        axes[-1].set_xticklabels(axes[-1].get_xticklabels(), rotation=30, ha='center')
-        comp.save(format=figformat)
+        
+        ax = go.Figure()
+        
+        for d in df["dataset"].unique():
+            ax.add_trace(go.Violin(x=df[y][df['dataset'] == d],
+                                    name=d,
+                                    points=False))  
+            
+        ax.update_traces(orientation='h', side='positive', width=3, points=False)
+        ax.update_layout(xaxis_showgrid=False,plot_bgcolor='rgb(255,255,255)')        
+        ax.update_xaxes(showline=True, linecolor='black')
+        
+        comp.fig = ax
+        comp.html = comp.fig.to_html(full_html=False, include_plotlyjs='cdn')
+        comp.save()
+    
     else:
         logging.error("Unknown comp plot type {}".format(plot))
         sys.exit("Unknown comp plot type {}".format(plot))
-    plt.close("all")
+    
     return [comp]
 
 
-def process_violin_and_box(ax, log, plot_obj, title, y_name, figformat, ymax):
+def process_violin_and_box(ax, log, plot_obj, title, y_name, ymax):
     if log:
         ticks = [10**i for i in range(10) if not 10**i > 10 * (10**ymax)]
-        ax.set(yticks=np.log10(ticks),
-               yticklabels=ticks)
-    ax.set(title=title or plot_obj.title,
-           ylabel=y_name)
-    plt.xticks(rotation=30, ha='center')
-    plot_obj.fig = ax.get_figure()
-    plot_obj.save(format=figformat)
-
-
-def output_barplot(df, figformat, path, title=None, palette=None):
+        ax.update_layout(
+            yaxis = dict(
+                tickmode = 'array',
+                tickvals = np.log10(ticks),
+                ticktext = ticks,
+                tickangle = 45
+            )
+        )
+        
+    ax.update_layout(
+        title_text=title or plot_obj.title,
+        title_x=0.5,  
+        yaxis_title=y_name,
+    )
+    
+    plot_obj.fig = ax
+    plot_obj.html = plot_obj.fig.to_html(full_html=False, include_plotlyjs='cdn')
+    plot_obj.save()
+    
+def output_barplot(df,path):
     """Create barplots based on number of reads and total sum of nucleotides sequenced."""
     logging.info("NanoComp: Creating barplots for number of reads and total throughput.")
-    read_count = Plot(path=path + "NanoComp_number_of_reads." + figformat,
+    read_count = Plot(path=path + "NanoComp_number_of_reads.html",
                       title="Comparing number of reads")
-    ax = sns.countplot(x="dataset",
-                       data=df,
-                       palette=palette)
-    ax.set(ylabel='Number of reads',
-           title=title or read_count.title)
-    plt.xticks(rotation=30, ha='center')
-    read_count.fig = ax.get_figure()
-    read_count.save(format=figformat)
-    plt.close("all")
+    
+    read_count.fig = go.Figure()
+    read_count.fig.add_trace(go.Histogram(histfunc="count",  x=list(df["dataset"])))
+    
+    read_count.fig.update_layout(
+        title_text=read_count.title,
+        title_x=0.5,  
+        yaxis_title="Number of reads",
+    )
+    
+    read_count.fig.update_xaxes(tickangle = 45)    
+    
+    read_count.html = read_count.fig.to_html(full_html=False, include_plotlyjs='cdn')
+    read_count.save()    
 
-    throughput_bases = Plot(path=path + "NanoComp_total_throughput." + figformat,
-                            title="Comparing throughput in gigabases")
+    throughput_bases = Plot(path=path + "NanoComp_total_throughput.html",
+                            title="Comparing throughput in bases")
     if "aligned_lengths" in df:
         throughput = df.groupby('dataset')['aligned_lengths'].sum()
-        ylabel = 'Total gigabase aligned'
+        ylabel = 'Total bases aligned'
     else:
         throughput = df.groupby('dataset')['lengths'].sum()
-        ylabel = 'Total gigabase sequenced'
-    ax = sns.barplot(x=list(throughput.index),
-                     y=throughput / 1e9,
-                     palette=palette,
-                     order=df["dataset"].unique())
-    ax.set(ylabel=ylabel,
-           title=title or throughput_bases.title)
-    plt.xticks(rotation=30, ha='center')
-    throughput_bases.fig = ax.get_figure()
-    throughput_bases.save(format=figformat)
-    plt.close("all")
+        ylabel = 'Total bases sequenced'
+    
+    throughput_bases.fig = go.Figure()
+    throughput_bases.fig.add_trace(go.Bar(x=list(df["dataset"].unique()), y=throughput))
+    
+    throughput_bases.fig.update_layout(
+        title_text=throughput_bases.title,
+        title_x=0.5,  
+        yaxis_title=ylabel,
+    )
+    
+    throughput_bases.fig.update_xaxes(tickangle = 45)    
+    
+    throughput_bases.html = throughput_bases.fig.to_html(full_html=False, include_plotlyjs='cdn')
+    throughput_bases.save()  
+    
     return read_count, throughput_bases
 
-
-def n50_barplot(df, figformat, path, title=None, palette=None):
-    n50_bar = Plot(path=path + "NanoComp_N50." + figformat,
+def n50_barplot(df, path):
+    '''
+    Returns Plot object and creates png/html containing bar chart of total gb aligned/sequenced read length n50
+    '''
+    n50_bar = Plot(path=path + "NanoComp_N50.html",
                    title="Comparing read length N50")
     if "aligned_lengths" in df:
         n50s = [get_N50(np.sort(df.loc[df["dataset"] == d, "aligned_lengths"]))
@@ -126,16 +158,20 @@ def n50_barplot(df, figformat, path, title=None, palette=None):
         n50s = [get_N50(np.sort(df.loc[df["dataset"] == d, "lengths"]))
                 for d in df["dataset"].unique()]
         ylabel = 'Sequenced read length N50'
-    ax = sns.barplot(x=list(df["dataset"].unique()),
-                     y=n50s,
-                     palette=palette,
-                     order=df["dataset"].unique())
-    ax.set(ylabel=ylabel,
-           title=title or n50_bar.title)
-    plt.xticks(rotation=30, ha='center')
-    n50_bar.fig = ax.get_figure()
-    n50_bar.save(format=figformat)
-    plt.close("all")
+    
+    n50_bar.fig = go.Figure()
+    n50_bar.fig.add_trace(go.Bar(x=list(df["dataset"].unique()), y=n50s))
+    
+    n50_bar.fig.update_layout(
+        title_text=n50_bar.title,
+        title_x=0.5,  
+        yaxis_title=ylabel,
+    )
+    
+    n50_bar.fig.update_xaxes(tickangle = 45)
+    
+    n50_bar.html = n50_bar.fig.to_html(full_html=False, include_plotlyjs='cdn')
+    n50_bar.save()
     return [n50_bar]
 
 
