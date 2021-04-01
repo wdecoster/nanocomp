b94b1cb4e68407a303bc8a9ccb966da9c366e8cd
wdecoster <decosterwouter@gmail.com>
2021-04-01 10:40:13 +0200

    change test to check if nanotest dir exists and write all to -o tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,16 @@
 set -ev
 
-git clone https://github.com/wdecoster/nanotest.git
+
+if [ -d "nanotest" ]; then
+  echo "nanotest already cloned"
+else
+  git clone https://github.com/wdecoster/nanotest.git
+fi
 
 NanoComp -h
-NanoComp --bam nanotest/alignment.bam nanotest/alignment.bam nanotest/alignment.bam --outdir compare-bams -f pdf -n run1 run2 run3
-NanoComp --summary nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt -n A B C
-NanoComp --fastq nanotest/reads.fastq.gz nanotest/reads.fastq.gz --names run1 run2 --plot box
-NanoComp --fasta nanotest/reads.fa.gz nanotest/reads.fa.gz nanotest/reads.fa.gz nanotest/reads.fa.gz --n run1 run2 run3 run4 --plot violin
-NanoComp --summary nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt -n A B --maxlength 20000
+NanoComp --bam nanotest/alignment.bam nanotest/alignment.bam nanotest/alignment.bam -f pdf -n run1 run2 run3 -o tests
+NanoComp --summary nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt -n A B C -o tests
+NanoComp --fastq nanotest/reads.fastq.gz nanotest/reads.fastq.gz --names run1 run2 --plot box -o tests
+NanoComp --fasta nanotest/reads.fa.gz nanotest/reads.fa.gz nanotest/reads.fa.gz nanotest/reads.fa.gz --n run1 run2 run3 run4 --plot violin -o tests
+NanoComp --summary nanotest/sequencing_summary.txt nanotest/sequencing_summary.txt -n A B --maxlength 20000 -o tests
 #NanoComp --feather nanotest/summary1.feather nanotest/summary2.feather nanotest/summary3.feather -n A B C
