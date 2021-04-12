6839182a4386517f4865eff8c66695c2698002e5
iliasbukraa <ilias.bukraa@gmail.com>
2021-04-12 18:24:32 +0200

    added html styling (same as NanoPlot)

--- nanocomp/NanoComp.py
+++ nanocomp/NanoComp.py
@@ -190,19 +190,93 @@ def make_report(plots, path, stats_df):
         <head>
         <meta charset="UTF-8">
             <style>
-            table, th, td {
-                text-align: left;
-                padding: 2px;
-                /* border: 1px solid black;
-                border-collapse: collapse; */
-            }
-            h2 {
-                line-height: 0pt;
-            }
-            </style>
+            body {margin:0}
+.hiddentitle { /* hides titles that are not necessary for content, but are for outline */
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  left: -10000px;
+}
+
+h1 { color: #111; font-family: 'Helvetica Neue', sans-serif; font-size: 60px; font-weight: bold; letter-spacing: -1px; line-height: 1; text-align: center; }
+
+h2 { color: #111; font-family: 'Open Sans', sans-serif; font-size: 25px; font-weight: 300; line-height: 32px; text-align: center; padding-bottom: 0;}
+
+h3 { color: #111; font-family: 'Helvetica Neue', sans-serif; font-size: 16px; font-weight: 150; margin: 0 0 0 0; text-align: left; padding:20px 0px 20px 0px;}
+
+table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  margin: 15px;
+  table-layout: auto;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+table td, table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+table tr:nth-child(even){background-color: #f2f2f2;}
+
+table tr:hover {background-color: #ddd;}
+
+/* Style the button that is used to open and close the collapsible content */
+.collapsible {
+  background-color: #39CCCC;
+  color: white;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active, .collapsible:hover {
+    color:white;
+  background-color: #001f3f;
+}
+
+/* Style the collapsible content. Note: hidden by default */
+.collapsible-content {
+  padding: 0 18px;
+  display: block;
+  overflow: hidden;
+  background-color: #FFFFFF;
+}
+
+.collapsible:after {
+  content: '-';
+  font-size: 20px;
+    font-weight: bold;
+  float: right;
+    color:white;
+  margin-left: 5px;
+}
+
+.active:after {
+  content: '+'; /* Unicode character for "minus" sign (-) */
+      color: white;
+
+}
+
+.hiddentitle { /* hides titles that are not necessary for content, but are for outline */
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  left: -10000px;
+}
+</style>
             <title>NanoComp Report</title>
         </head>"""
-    html_content = ["\n<body>\n<h1>NanoComp report</h1>"]
+
+    html_content = ["<body><h1>NanoComp report</h1>"]
     html_content.append("<h2>Summary statistics</h2>")
     if stats_df is not None:
         html_content.append(stats_df.to_html())
@@ -210,9 +284,20 @@ def make_report(plots, path, stats_df):
         html_content.append(utils.stats2html(path + "NanoStats.txt"))
     html_content.append('\n<br>\n<br>\n<br>\n<br>')
     html_content.append("<h2>Plots</h2>")
+
     for plot in plots:
-        html_content.append("\n<h3>" + plot.title + "</h3>\n" + plot.encode())
-        html_content.append('\n<br>\n<br>\n<br>\n<br>')
+        html_content.append('<button class="collapsible">' + plot.title + '</button>')
+        html_content.append('<section class="collapsible-content"><h4 class="hiddentitle" id="' +
+                            plot.title.replace(' ', '_') + '">' + plot.title + '</h4>')
+        html_content.append(plot.encode())
+        html_content.append('</section>')
+
+    html_content.append(
+        '<script>var coll = document.getElementsByClassName("collapsible");var i;for (i = 0; i < coll.length; i++) {'
+        'coll[i].addEventListener("click", function() {this.classList.toggle("active");var content = '
+        'this.nextElementSibling;if (content.style.display === "none") {content.style.display = "block";} else {'
+        'content.style.display = "none";}});}</script>')
+
     html_body = '\n'.join(html_content) + "</body></html>"
     html_str = html_head + html_body
     with open(path + "NanoComp-report.html", "w") as html_file:
